192c523c6985cf2d94480ecc316ad146d7ac38c7
Kuzanity <adriangabrieldiaz07@gmail.com>
2021-11-20 02:01:34 +0000

    Activity

---
+++ tempdir/design_app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000)
